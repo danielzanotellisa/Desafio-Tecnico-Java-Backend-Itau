bf91d0065cda48f6a1a4e64375bafcf295dafeda
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-26 19:58:03 -0300

    build: arquivos de build

---
+++ src/test/java/dev/daniel/zanotelli/ItauTesteTecnico/ItauTesteTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.daniel.zanotelli.ItauTesteTecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauTesteTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
