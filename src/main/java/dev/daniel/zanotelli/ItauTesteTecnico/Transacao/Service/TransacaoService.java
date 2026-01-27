3a5c0f5b5d53c86343050e90a479d5387ea636ef
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-27 10:24:00 -0300

    refactor: remove validações que agora são feitas no `ExcepetionHandlerHelper`

--- src/main/java/dev/daniel/zanotelli/ItauTesteTecnico/Transacao/Service/TransacaoService.java
+++ src/main/java/dev/daniel/zanotelli/ItauTesteTecnico/Transacao/Service/TransacaoService.java
@@ -1,7 +1,7 @@
 package dev.daniel.zanotelli.ItauTesteTecnico.Transacao.Service;
 
 import dev.daniel.zanotelli.ItauTesteTecnico.Transacao.Request.TransacaoRequest;
-import dev.daniel.zanotelli.ItauTesteTecnico.Transacao.Exceptions.UnprocessableEntity;
+import dev.daniel.zanotelli.ItauTesteTecnico.Transacao.Exceptions.UnprocessableContent;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -12,20 +12,12 @@ public class TransacaoService {
 
     public void validate(TransacaoRequest transacaoRequest) {
 
-        if(transacaoRequest.valor() == null) {
-            throw new UnprocessableEntity("valor");
-        }
-
-        if(transacaoRequest.dataHora() == null) {
-            throw new UnprocessableEntity("dataHora");
-        }
-
         if(transacaoRequest.valor().compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("O valor deve ser maior ou igual a zero");
+            throw new UnprocessableContent("O valor deve ser maior ou igual a zero");
         }
 
         if(transacaoRequest.dataHora().isAfter(OffsetDateTime.now())) {
-            throw new IllegalArgumentException("A data da transação não pode ser no futuro");
+            throw new UnprocessableContent("A data da transação não pode ser no futuro");
         }
     }
 }
