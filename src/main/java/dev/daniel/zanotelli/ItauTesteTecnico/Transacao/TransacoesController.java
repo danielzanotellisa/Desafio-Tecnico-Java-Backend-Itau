a838b128ea12bbfde39b7683fa9d5e7e4874eaaf
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-26 21:59:41 -0300

    feat: faz injecao de dependencia e chama metodo `validate` do service

--- src/main/java/dev/daniel/zanotelli/ItauTesteTecnico/Transacao/TransacoesController.java
+++ src/main/java/dev/daniel/zanotelli/ItauTesteTecnico/Transacao/TransacoesController.java
@@ -1,5 +1,7 @@
 package dev.daniel.zanotelli.ItauTesteTecnico.Transacao;
 
+import dev.daniel.zanotelli.ItauTesteTecnico.Transacao.Request.TransacaoRequest;
+import dev.daniel.zanotelli.ItauTesteTecnico.Transacao.Service.TransacaoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/transacao")
 public class TransacoesController {
 
+    private final TransacaoService service;
+    public TransacoesController(TransacaoService service) {
+        this.service = service;
+    }
+
     @PostMapping()
-    public ResponseEntity criarTransacao(RequestBody T) {
+    public ResponseEntity criarTransacao(@RequestBody TransacaoRequest transacaoRequest) {
+
+        service.validate(transacaoRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
