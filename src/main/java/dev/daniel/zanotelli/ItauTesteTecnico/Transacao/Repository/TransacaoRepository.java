ccdf3e34c2113fea6facc101175def7e46dcdcf7
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-27 20:23:44 -0300

    refactor: adiciona anotação `repository`

--- src/main/java/dev/daniel/zanotelli/ItauTesteTecnico/Transacao/Repository/TransacaoRepository.java
+++ src/main/java/dev/daniel/zanotelli/ItauTesteTecnico/Transacao/Repository/TransacaoRepository.java
@@ -2,16 +2,19 @@ package dev.daniel.zanotelli.ItauTesteTecnico.Transacao.Repository;
 
 
 import dev.daniel.zanotelli.ItauTesteTecnico.Transacao.Request.TransacaoRequest;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class TransacaoRepository {
 
     List<TransacaoRequest> transacoes = new ArrayList<>();
 
     public void salvarDados(TransacaoRequest transacao){
         transacoes.add(transacao);
+        System.out.println(this.transacoes);
     }
 
     public void limparDados() {}
