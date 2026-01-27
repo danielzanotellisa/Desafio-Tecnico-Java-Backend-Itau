ff5a85809afa07820c58e1402974f41ca275fe44
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-27 10:22:37 -0300

    feat: anota campos com `@NotNull` e mensagem de erro

--- src/main/java/dev/daniel/zanotelli/ItauTesteTecnico/Transacao/Request/TransacaoRequest.java
+++ src/main/java/dev/daniel/zanotelli/ItauTesteTecnico/Transacao/Request/TransacaoRequest.java
@@ -1,7 +1,10 @@
 package dev.daniel.zanotelli.ItauTesteTecnico.Transacao.Request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
-public record TransacaoRequest(BigDecimal valor, OffsetDateTime dataHora) {
+public record TransacaoRequest(@NotNull(message = "O campo valor deve estar presente") BigDecimal valor,
+                               @NotNull(message = "O campo dataHora deve estar presente") OffsetDateTime dataHora) {
 }
